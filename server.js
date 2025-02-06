35707aacf774da9da609dd299b225f83ec472d1a
SANJAY S <161963402+SANJAY1574@users.noreply.github.com>
2025-02-06 22:05:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,4 +91,6 @@ app.post("/webhook", (req, res) => {
 });
 
 // ✅ Start Server
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+
